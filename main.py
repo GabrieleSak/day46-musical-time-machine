e2403f6b7e5b83b3f435b444e8d17fc77cd7021e
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-02-14 10:04:18 +0200

    Scrape top 100 song titles on selected date into a list

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
 date = input("Which date do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-print(date)
+
+URL = f"https://www.billboard.com/charts/hot-100/{date}/"
+
+response = requests.get(URL)
+
+bilboard_hot_100_web_page = response.text
+
+soup = BeautifulSoup(bilboard_hot_100_web_page, "html.parser")
+
+songs_list = soup.find_all(name="h3", class_='a-no-trucate')
+
+songs = [song.getText().strip() for song in songs_list]
+
+print(songs)
